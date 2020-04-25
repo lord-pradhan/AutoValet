c183726e8a6e8b6a2ebed2ad3bb1c07c8dab39b0
Roshan <roshannpradhan@gmail.com>
2020-04-25 17:30:27 -0400

    made new package for global planner plugin

--- build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -9,7 +9,8 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/lord-pradhan/auto_valet/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/opt/ros/melodic/lib:/usr/local/cuda-9.0/lib64:/usr/local/cuda/lib64:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
-export PATH="/opt/ros/melodic/bin:/usr/local/cuda-9.0/bin:/home/lord-pradhan/anaconda3/condabin:/home/lord-pradhan/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/opt/ros/melodic/lib/pkgconfig"
 export PWD="/home/lord-pradhan/auto_valet/build/catkin_tools_prebuild"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/lord-pradhan/auto_valet/devel/.private/catkin_tools_prebuild/share/common-lisp"
+export PYTHONPATH="/opt/ros/melodic/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lord-pradhan/auto_valet/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/lord-pradhan/auto_valet/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
